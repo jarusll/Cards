ff9d0635ed9158639a375fc4b73a9a91399c7eeb
Suraj Yadav <ysuraj877@gmail.com>
2022-07-12 02:07:01 +0530

    card finalize

--- scenes/Card.gd
+++ scenes/Card.gd
@@ -1,17 +1,16 @@
-extends CanvasLayer
+extends Area2D
 class_name Card 
 
-var ability_name = "ability"
-var character_name = "character"
-var description_text = "description"
-
-func _init():
-	return 
+var ability_name = "FRAG/ment"
+var character_name = "KAY/O"
+var description_text = "Explosive which bursts on sticking"
 
 func _ready():
 	$AbilityLabel.set_text(ability_name)
 	$CharacterLabel.set_text(character_name)
 	$DescriptionLabel.set_text(description_text)
+	var screensize = get_viewport().get_visible_rect().size	
+	position = Vector2(screensize.x/2, screensize.y/2)
 
 func set_ability(ability):
 	ability_name = ability
